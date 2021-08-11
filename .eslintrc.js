8d7dcdebdcba67ff72dafbac0a984ecf7545b50a
Kryštof Rainisch <stofi@users.noreply.github.com>
2021-08-11 10:00:17 +0200

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+    parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+
+    parserOptions: {
+        ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
+
+        sourceType: 'module', // Allows for the use of imports
+
+        project: './tsconfig.json',
+    },
+
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'prettier/@typescript-eslint',
+        'plugin:prettier/recommended',
+    ],
+
+    rules: {
+        // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+        // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+    },
+}
